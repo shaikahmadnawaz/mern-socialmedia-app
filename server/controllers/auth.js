d70ebd689dfa3e157dc386b3c6db66e8b4be606d
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2022-12-13 16:47:23 +0530

    assets

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,35 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User";
+
+// register user
+// The req. body object allows you to access data in a string or JSON object from the client side. You generally use the req. body object to receive data through POST and PUT requests in the Express server.
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      friends,
+      location,
+      occupation,
+    } = req.body();
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+  } catch (err) {}
+};
