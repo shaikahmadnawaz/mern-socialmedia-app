5901a50c01160425704a24be703c649f64f336f3
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2022-12-14 17:48:16 +0530

    routes

--- server/index.js
+++ server/index.js
@@ -9,6 +9,9 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routers/auth";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
 
 // configurations
 // This is only implemented when ypu use type module in package.json
@@ -57,6 +60,8 @@ app.post("/auth/register", upload.single("picture"), register);
 
 // routes
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // mongoose setup
 const PORT = process.env.PORT || 6001;
