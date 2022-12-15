658af27c0f5178d52d8bd36dcb8235b3a393ae60
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2022-12-15 16:24:40 +0530

    folders created

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
+import { BrowserRouter, Router, Routes } from "react-router-dom";
+import HomePage from "./scenes/homePage";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="app"></div>;
 }
 
 export default App;
