d2e199040992942173c4045d8b5115fa18d87842
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2022-12-15 16:32:23 +0530

    routes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
-import { BrowserRouter, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./scenes/homePage";
 import LoginPage from "./scenes/loginPage";
 import ProfilePage from "./scenes/profilePage";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
